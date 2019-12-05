6e07864fb533fa829279368495c358552e5efbb8
Gulik17 <gulik.v@bk.ru>
2019-12-05 20:17:43 +0300

    Повторный комит ДЗ 6

--- 6|_ГулеватыйВладимир.playground/Contents.swift
+++ 6|_ГулеватыйВладимир.playground/Contents.swift
@@ -1,117 +1,59 @@
 import UIKit
 
-/*
- 1. Создать протокол «Car» и описать свойства, общие для автомобилей, а также метод действия.
- 2. Создать расширения для протокола «Car» и реализовать в них методы конкретных действий с автомобилем: открыть/закрыть окно, запустить/заглушить двигатель и т.д. (по одному методу на действие, реализовывать следует только те действия, реализация которых общая для всех автомобилей).
- 3. Создать два класса, имплементирующих протокол «Car» - trunkCar и sportСar. Описать в них свойства, отличающиеся для спортивного автомобиля и цистерны.
- 4. Для каждого класса написать расширение, имплементирующее протокол CustomStringConvertible.
- 5. Создать несколько объектов каждого класса. Применить к ним различные действия.
- 6. Вывести сами объекты в консоль. */
+/**
+ * Практическое задание 6
+ *
+ * 1. Реализовать свой тип коллекции «очередь» (queue) c использованием дженериков.
+ * 2. Добавить ему несколько методов высшего порядка, полезных для этой коллекции (пример: filter для массивов)
+ * 3. Добавить свой subscript, который будет возвращать nil в случае обращения к несуществующему индексу.
+ */
 
-protocol CarProtocol {
-    var brand: String {get}
-    var color: String {get}
-    var issue: UInt {get}
+struct queue<T> {
+    var array: [T] = []
 
-    func engineStart() -> String
-    func engineStop() -> String
-    func windowsOpen() -> String
-    func windowsClose() -> String
-}
-
-extension CarProtocol {
-    func engineStart() -> String {
-        return "Start"
-    }
-    func engineStop() -> String {
-        return "Stop"
-    }
-    func windowsOpen() -> String {
-        return "Open"
-    }
-    func windowsClose() -> String {
-        return "Close"
-    }
-}
-
-// класс машины
-class trunkCar: CarProtocol {
-    enum truck: String {
-        case Open, Close
+    mutating func push(_ item: T) {
+        array.append(item)
     }
 
-    var brand: String
-    var color: String
-    var issue: UInt
-    var trunkVol: Int
-    var doorStatus: String
-    var truckStatus: truck = .Close
-    
-    init(brand: String, color: String, issue: UInt, trunkVol: Int, doorStatus: String, truckStatus: truck) {
-        self.brand = brand
-        self.color = color
-        self.issue = issue
-        self.trunkVol = trunkVol
-        self.doorStatus = doorStatus
-        self.truckStatus = truckStatus
+    mutating func pop() -> T? {
+        return array.removeLast()
     }
-    
-    func openCargo() {
-        truckStatus = .Open
+
+    mutating func filter(order: (T) -> Bool ) -> [T] {
+        var temp = [T]()
+        for item in array {
+            if order(item) {
+                temp.append(item)
+            }
+        }
+        return temp
     }
-    func closeCargo() {
-        truckStatus = .Close
+
+    subscript(idx: Int) -> T? {
+        return (idx < self.array.count) ? array[idx] : nil
     }
 }
 
-class sportCar: CarProtocol {
-    enum roof: String {
-        case Open, Close
-    }
-
-    var brand: String
-    var color: String
-    var issue: UInt
-    var trunkVol: Int
-    var doorStatus: String
-    var roofStatus: roof = .Close
-    
-    init(brand: String, color: String, issue: UInt, trunkVol: Int, doorStatus: String, roofStatus: roof) {
-        self.brand = brand
-        self.color = color
-        self.issue = issue
-        self.trunkVol = trunkVol
-        self.doorStatus = doorStatus
-        self.roofStatus = roofStatus
-    }
-    
-    func openRoof() {
-        roofStatus = .Open
-    }
-    func closeRoof() {
-        roofStatus = .Close
-    }
-
+let odd: (Int) -> Bool = { (val: Int) -> Bool in
+    return val % 2 == 0
 }
 
-extension trunkCar: CustomStringConvertible {
-    var description: String{
-        return "Машина - \(brand) \nцвет - \(color) \n\(issue) года выпуска \nобъемом кузова - \(trunkVol)\nкузов - \(truckStatus)\n"
-    }
+let even: (Int) -> Bool = { (val: Int) -> Bool in
+    return val % 2 != 0
 }
 
-extension sportCar: CustomStringConvertible {
-    var description: String{
-        return "Машина - \(brand) \nцвет - \(color) \n\(issue) года выпуска \nобъемом багажника - \(trunkVol)\nкрыша - \(roofStatus)\n"
-    }
-}
+var queue_int = queue<Int>()
+queue_int.push(1)
+queue_int.push(2)
+queue_int.push(3)
+queue_int.push(4)
+queue_int.push(5)
+print(queue_int)
+print("\(String(describing: queue_int[55])) - обращаемся к несуществующему элементу массива")
 
-var opel = sportCar(brand: "Opel", color: "Серый", issue: 2005, trunkVol: 500, doorStatus: "Closed", roofStatus: .Close)
-var kamaz = trunkCar(brand: "Kamaz", color: "Красный", issue: 1985, trunkVol: 10000, doorStatus: "Closed", truckStatus: .Close)
-
-opel.roofStatus = .Open
-print(opel)
-kamaz.truckStatus = .Open
-print(kamaz)
-var opelEngine = opel.engineStart()
-print("Двигатель \(opel.brand) - \(opelEngine)")
+var queue_odd = queue_int.filter(order: odd)
+print(queue_odd)
+var queue_even = queue_int.filter(order: even)
+print(queue_even)
+queue_int.pop()
+print("\(String(describing: queue_int[5])) - проверяем удаленный элементу массива")
